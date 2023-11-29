a31677faf6acfe28e15d2f9c3968042496b3ff52
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-11-28 21:06:56 -0500

    Updates seed.

--- db/seed.sql
+++ db/seed.sql
@@ -7,7 +7,7 @@ VALUES
 INSERT INTO destinations (destination_name, image_url)
 VALUES
 ('Japan', 'https://images.pexels.com/photos/402028/pexels-photo-402028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-('Costa Rica', 'https://images.pexels.com/photos/17302348/pexels-photo-17302348/free-photo-of-a-beach-with-palm-trees-and-sand.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+('Costa Rica', 'https://images.pexels.com/photos/14580735/pexels-photo-14580735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
 ('Rome', 'https://images.pexels.com/photos/161858/rome-ancient-italy-landmark-161858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
 
 INSERT INTO memories (rating, cost, review, experiences, date, travel_user_id, destination_id)
